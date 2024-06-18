7e6e0fa60f965783c5e231f70e3f5dfdb9bb3c2f
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-06-18 23:21:22 +0530

    Events functions and states

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,29 @@
+"use client"
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const [name, setName] = useState("Amul")
+
+  const apple = (item) => {
+    // alert(item)
+    setName("Biswas")
+  }
+
+  const Innercomp = () => {
+    return (
+      <h1>Inner Component</h1>
+    )
+  }
+
   return (
     <main className={styles.main}>
-      <User name="Nitesh Kr Singh" />
-      <User name="Annu" />
-      <User name="Chotu" />
-      <User name="bhawani" />
-      <h1>HomePage</h1>
+      <h1>Event, functions and state {name}</h1>
+      <button onClick={apple}>Click Me</button>
+      <Innercomp />
     </main>
   );
 }
 
 
-const User = ({ name }) => {
-  return (
-    <div>
-      <h2>User name is {name}</h2>
-    </div>
-  )
-}
