bf575686e0dda2562778847215cc0371a3fb607c
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-07-06 19:09:45 +0530

    all segments

---
+++ src/app/study/[...lecture]/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Lecture() {
+    return (
+        <>
+            <div>Lecture 1 of College</div>
+            <div>Lecture 2 of College</div>
+            <div>Lecture 2 of College</div>
+        </>
+    )
+}
+
+export default Lecture
---
+++ src/app/study/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Study() {
+    return (
+        <div>
+            <h1>All lectures of College</h1>
+        </div>
+    )
+}
+
+export default Study
