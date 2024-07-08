f9fe0dc9173fc6e8be49facab11c34f8558600b9
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-07-08 11:15:52 +0530

    deleted file for CSS tutorial

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,8 @@
-"use client"
-
-import Link from "next/link";
-
 export default function Home() {
 
   return (
     <main >
-      <h1>Fetch Data with API in Client Component</h1>
-      <Link href="/productlist">Go to Client API Fetching</Link>
-      <br />
-      <Link href="/productlist-server"> Go to Server API fetching</Link>
+      <h1>Style and CSS with NEXTjs</h1>
     </main>
   );
 }
--- src/app/productlist-server/product.js
+++
@@ -1,8 +0,0 @@
-"use client"
-export default function Product({ price }) {
-    return (
-        <div>
-            <button onClick={() => alert(price)}>Click Me</button>
-        </div>
-    )
-}
